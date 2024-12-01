9cf12d8cce1c720f4a1d4b23e602377b537f32e9
Reed Streets <rstreets96@gmail.com>
2024-12-01 15:52:02 -0800

    Day 1 python solution completed. May come back for cleanup

--- python/Day1.py
+++ python/Day1.py
@@ -1,5 +1,6 @@
 import logging
 import argparse
+import numpy as np
 
 import submodules.utils as utils
 
@@ -12,9 +13,39 @@ if __name__ == "__main__":
     utils.common_args(parser=parser)
     args = parser.parse_args()
   
-    logger.info(f"Opening {args.filename}")
+    logger.debug(f"Opening {args.filename}")
 
     with open(args.filename, "r") as f:
         input = f.readlines()
     
-    print(input)
+    for i in range(len(input)):
+        input[i] = input[i].split()
+    
+    input_array = np.array(input)
+    lists = input_array.T
+
+    # Part 1
+    #************************************************************
+    if 1 in args.part_nums:
+        list_copy = lists
+        list_copy[0].sort()
+        list_copy[1].sort()
+        sum = 0
+        for i in range(len(list_copy[0])):
+            sum += abs(int(list_copy[0][i]) - int(list_copy[1][i]))
+        logger.info(f"Solution to part 1: {sum}")
+
+    # Part 2
+    #************************************************************
+    if 2 in args.part_nums:
+        list2_dict = {}
+        for item in lists[1]:
+            if item in list2_dict.keys():
+                list2_dict[item] +=1
+            else:
+                list2_dict[item] = 1
+        sum = 0
+        for item in lists[0]:
+            if item in list2_dict.keys():
+                sum += int(item) * list2_dict[item]
+        logger.info(f"Solution to part 2: {sum}")
--- python/submodules/utils.py
+++ python/submodules/utils.py
@@ -2,3 +2,4 @@ import argparse
 
 def common_args(parser):
     parser.add_argument("-f", "--filename", type=str, required=True, help="The relative filepath to the input file")
+    parser.add_argument("-p", "--part-nums", type=int, nargs='+', default=[1, 2], help="Choose which parts of the problem to execute")
