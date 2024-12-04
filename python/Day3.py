7a73e804cacd4b2d59902d2c1909d171d364af30
Reed Streets <rstreets96@gmail.com>
2024-12-03 19:20:02 -0800

    Day 3: remember previous search index

--- python/Day3.py
+++ python/Day3.py
@@ -6,39 +6,19 @@ import submodules.utils as utils
 
 logger = logging.getLogger(__name__)
 
-# def find_closest(num_list, num):
-#     if num < num_list[0]:
-#         return None
-#     elif num > num_list[-1]:
-#         return num_list[-1]
-#     timeout = 0
-#     i = len(num_list) // 2
-#     while not (num_list[i] < num < num_list[i+1]) and timeout < 20:
-#         if num > num_list[i]:
-#             i += (len(num_list) - i + 1) // 2
-#         else:
-#             i = (i - 1) // 2
-#         timeout += 1
 
-#     if num_list[i] < num < num_list[i+1]:
-#         logger.debug(f"Found! {i}, {num_list[i]}, {num}")
-#         return num_list[i]
-#     else:
-#         logger.debug(f"Not Found! List: {num_list} Num: {num}")
-#         return None
-
-def find_closest(num_list, num):
+def find_closest(num_list, num, start):
     if num < num_list[0]:
         return None
     elif num > num_list[-1]:
-        return num_list[-1]
-    for i in range(len(num_list)):
+        return len(num_list) - 1
+    for i in range(start, len(num_list)):
         if num_list[i] < num < num_list[i+1]:
-            return num_list[i]
+            return i
     return None
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser(prog='Day3', description='Python solution to Advent of Code Day 3 puzzle')
     utils.common_args(parser=parser)
@@ -67,19 +47,33 @@ if __name__ == "__main__":
     dont_matches = re.finditer(pattern_dont, input)
     do_idxs = []
     dont_idxs = []
-    sum = 0
 
     for match in do_matches:
         do_idxs.append(match.span()[0])
     for match in dont_matches:
         dont_idxs.append(match.span()[0])
+
+    prev_do_idx = 0
+    do_idxs_i = 0
+    prev_dont_idx = 0
+    dont_idxs_i = 0
+
+    sum = 0
     for match in mul_matches:
-        prev_do_idx = find_closest(do_idxs, match.span()[0])
-        if prev_do_idx == None:
+        do_idxs_i = find_closest(do_idxs, match.span()[0], do_idxs_i)
+        if do_idxs_i == None:
+            do_idxs_i = 0
             prev_do_idx = 0
-        prev_dont_idx = find_closest(dont_idxs, match.span()[0])
-        if prev_dont_idx == None:
+        else:
+            prev_do_idx = do_idxs[do_idxs_i]
+
+        dont_idxs_i = find_closest(dont_idxs, match.span()[0], dont_idxs_i)
+        if dont_idxs_i == None:
+            dont_idxs_i = 0
             prev_dont_idx = 0
+        else:
+            prev_dont_idx = dont_idxs[dont_idxs_i]
+
         logger.debug(f"Prev do(): {prev_do_idx}. Prev don't(): {prev_dont_idx}, Match: {match.span()[0]}, {sum}")
         if prev_do_idx > prev_dont_idx or prev_dont_idx == 0:
             pair = match.group()[4:-1].split(',')
